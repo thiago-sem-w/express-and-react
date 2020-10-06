0cea3e9670de501ef6ef72af6c9d104e42bf0242
Thiago Sem W <thiagosemw@gmail.com>
2020-10-05 23:23:42 -0300

    add loader jsx

--- webpack.config.js
+++ webpack.config.js
@@ -5,5 +5,16 @@ const path = require('path')
 module.exports = {
 	mode: 'development',
 	devtool: 'eval-source-map',
-	entry: path.resolve(__dirname, 'public', 'index.js')
+	entry: path.resolve(__dirname, 'public', 'index.jsx'),
+	module: {
+		rules: [
+			{
+				test: /\.(js|jsx)$/,
+				exclude: /node_modules/,
+				use: {
+					loader: 'babel-loader'
+				}
+			}
+		]
+	}
 }
